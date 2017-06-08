c7c8bd108c8305caf6913aa55100ce200e7634a3
Jared Hanson <jaredhanson@gmail.com>
2017-06-08 16:56:03 -0700

    Mirror directory structure in tests.

--- test/authentication/password/authenticate.test.js
+++ test/authentication/password/authenticate.test.js
@@ -2,10 +2,10 @@
 
 var expect = require('chai').expect;
 var sinon = require('sinon');
-var factory = require('../../app/authentication/password/authenticate');
+var factory = require('../../../app/authentication/password/authenticate');
 
 
-describe('password/authenticate', function() {
+describe('authentication/password/authenticate', function() {
   
   it('should export factory function', function() {
     expect(factory).to.be.a('function');
--- test/authentication/token/authenticate.test.js
+++ test/authentication/token/authenticate.test.js
@@ -2,10 +2,10 @@
 
 var expect = require('chai').expect;
 var sinon = require('sinon');
-var factory = require('../../app/authentication/token/authenticate');
+var factory = require('../../../app/authentication/token/authenticate');
 
 
-describe('token/authenticate', function() {
+describe('authentication/token/authenticate', function() {
   
   it('should export factory function', function() {
     expect(factory).to.be.a('function');
