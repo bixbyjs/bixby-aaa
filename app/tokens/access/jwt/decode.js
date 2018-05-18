e52d20619c957ff1a34ea18687b5d54d3733b89a
Jared Hanson <jaredhanson@gmail.com>
2018-05-18 06:58:16 -0700

    Use implementation from tokens package.

--- app/tokens/access/jwt/decode.js
+++ app/tokens/access/jwt/decode.js
@@ -1,22 +1,6 @@
 exports = module.exports = function() {
+  // Load modules.
+  var tokens = require('tokens');
   
-  
-  return function decode(claims, options, cb) {
-    console.log('INTERPRET JWT!!!!');
-    console.log(claims);
-    console.log(options);
-    
-    if (!(claims.iss || claims.sub || claims.aud)) {
-      // not a dialect we understand
-      return cb();
-    }
-    
-    console.log('IS A JWT!');
-    
-    
-    var params = {};
-    params.subject = { id: claims.sub };
-    params.client = { id: claims.cid };
-    return cb(null, params);
-  };
+  return tokens.jwt.interpret();
 };
