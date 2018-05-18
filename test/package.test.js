4809b859c816bd3c128efc2a3115e73d59985cfc
Jared Hanson <jaredhanson@gmail.com>
2018-05-17 21:50:21 -0700

    Fix tests.

--- test/package.test.js
+++ test/package.test.js
@@ -11,7 +11,7 @@ describe('bixby-security', function() {
     it('should have assembly metadata', function() {
       expect(json.assembly.namespace).to.equal('security');
       
-      expect(json.assembly.components).to.have.length(4);
+      expect(json.assembly.components).to.have.length(5);
       expect(json.assembly.components).to.include('authentication/token/authenticate');
     });
   });
