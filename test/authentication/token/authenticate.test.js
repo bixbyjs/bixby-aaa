ff261ef71312b71f89c1b33135a5b87efcae3e4f
Jared Hanson <jaredhanson@gmail.com>
2018-05-25 11:57:11 -0700

    Add test.

--- test/authentication/token/authenticate.test.js
+++ test/authentication/token/authenticate.test.js
@@ -16,4 +16,50 @@ describe('authentication/token/authenticate', function() {
     expect(factory['@singleton']).to.be.undefined;
   });
   
+  describe('authenticate', function() {
+    var tokens = {
+      decode: function(){}
+    }
+    
+    
+    describe('valid token', function() {
+      var user, info;
+      
+      before(function() {
+        sinon.stub(tokens, 'decode').yields(null, { id: '501', username: 'johndoe' });
+      });
+      
+      after(function() {
+        tokens.decode.restore();
+      });
+      
+      before(function(done) {
+        var authenticate = factory(tokens);
+        
+        authenticate('2YotnFZFEjr1zCsicMWpAA', function(err, u, i) {
+          if (err) { return done(err); }
+          user = u;
+          info = i;
+          done();
+        })
+      });
+      
+      it('should decode token', function() {
+        expect(tokens.decode.callCount).to.equal(1);
+        expect(tokens.decode.args[0][0]).to.equal('2YotnFZFEjr1zCsicMWpAA');
+      });
+      
+      /*
+      it('should yield user', function() {
+        expect(user).to.deep.equal({ id: '501', username: 'johndoe' });
+      });
+      
+      it('should yield info', function() {
+        expect(info).to.deep.equal({ method: 'password' });
+      });
+      */
+    }); // valid token
+    
+  }); // authenticate
+  
 });
