495160904f383790f85716895d9c794af05f1a7c
Jared Hanson <jaredhanson@gmail.com>
2018-05-25 17:23:29 -0700

    Add  tests.

--- test/credentials/file.test.js
+++ test/credentials/file.test.js
@@ -1,5 +1,6 @@
 /* global describe, it */
 
+var path = require('path');
 var chai = require('chai');
 var expect = require('chai').expect;
 var sinon = require('sinon');
@@ -16,5 +17,32 @@ describe('credentials/file', function() {
     expect(factory['@implements']).to.be.undefined;
     expect(factory['@singleton']).to.be.undefined;
   });
+  
+  describe.only('FileCredentialStore', function() {
+    
+    describe('containing credentials referenced by identifier', function() {
+      var store = factory()('file://' + path.resolve(__dirname, '../fixtures/credentials/userinfo.toml'));
+      
+      describe('get credential for entity with which there is a single shared secret', function() {
+        var cred;
+        
+        before(function(done) {
+          store.get({ identifier: 'https://server.example.com' }, function(err, c) {
+            if (err) { return done(err); }
+            cred = c;
+            done();
+          });
+        });
+        
+        it('should yield credential', function() {
+          expect(cred).to.deep.equal({
+            secret: 'UyLvzezoJN0VpjegQMJg0akUuYq3VHft'
+          });
+        });
+      }); // get credential
+      
+    }); // containing credentials referenced by identifier
+    
+  }); // FileCredentialStore
   
 });
